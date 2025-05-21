505965671b7aa80c2ac28171f001ed515f633bc3
Diangou <diangou.cmr09@gmail.com>
2025-05-21 16:37:57 +0200

    Refonte du footer

--- squelette/footer.php
+++ squelette/footer.php
@@ -6,7 +6,23 @@
   <div class="ft-container">
     <div class="ft-grid">
       <!-- Image du logo -->
-      <img src="../../medias/logo_trasparent-decodumonde-2.png" alt="logo" class="ft-logo">
+    <a href="../pages/index.php">
+      <img src="../../medias/logo_blanc-decodumonde.png" alt="logo" class="ft-logo">
+    </a>
+
+<!-- Colonne carte Google Maps -->
+<div class="ft-map">
+  <iframe
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2619.750900292191!2d2.3551069767092527!3d48.9582288713459!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e6696a2070969b%3A0x328587555f1a855d!2sD%C3%A9co%20du%20Monde!5e0!3m2!1sfr!2sfr!4v1747834913503!5m2!1sfr!2sfr"
+    width="100%" height="180"
+    style="border:0; border-radius: 10px;"
+    allowfullscreen=""
+    loading="lazy"
+    referrerpolicy="no-referrer-when-downgrade"
+    title="Carte Deco du Monde">
+  </iframe>
+</div>
+
 
       <!-- Section adresse et contact -->
       <div class="ft-contact">
@@ -20,12 +36,22 @@
         <ol>
           <li class="ft-nav-item"><a href="../pages/index.php">Accueil</a></li>
           <li class="ft-nav-item"><a href="../EtapesPersonnalisation/etape1-1-structure.php">Personnalisation</a></li>
-          <li class="ft-nav-item"><a href="../pages/commandes.php">Commande</a></li>
-          <li class="ft-nav-item"><a href="../pages/aide.php">Besoin d'aide ?</a></li>
+          <li class="ft-nav-item"><a href="../EtapesPersonnalisation/etape1-1-structure.php">À Propos</a></li>
+          <li class="ft-nav-item"><a href="../pages/commandes.php">Mes Commandes</a></li>
         </ol>
       </nav>
+      
+      <nav class="ft-nav">
+        <ol>
+          <li class="ft-nav-item"><a href="../pages/aide.php">Besoin d'aide ?</a></li>
+          <li class="ft-nav-item"><a href="../pages/mentionslegales.php">Mentions Légales</a></li>
+          <li class="ft-nav-item"><a href="../pages/politiquedeconfidentialite.php">Politique de Confidentialité</a></li>
+
+        </ol>
+      </nav>
+      
     </div>
   </div>
-  <!-- Mention légale -->
-  <p class="ft-legal">© 2024 Deco du Monde - Tous droits réservés</p>
 </footer>
+<!-- Mention légale -->
+  <p class="ft-legal">© 2024 Deco du Monde - Tous droits réservés</p>
