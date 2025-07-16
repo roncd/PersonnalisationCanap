aa538f6d2a42e0cd355e0f3a0da057ad4375574f
Rosalie <rosalie.nicaud@gmail.com>
2025-07-16 11:29:17 +0200

    correction affichage pdf

--- front/generate-pdf/generer-devis-bois-mail.php
+++ front/generate-pdf/generer-devis-bois-mail.php
@@ -151,9 +151,9 @@ $pdf->Ln();
 // Table des produits commandés
 $pdf->SetFont('Arial', 'B', 12);
 $pdf->Cell(60, 10, "Produit", 1);
-$pdf->Cell(60, 10, mb_convert_encoding("Référence", "ISO-8859-1", "UTF-8"), 1);
+$pdf->Cell(80, 10, mb_convert_encoding("Référence", "ISO-8859-1", "UTF-8"), 1);
 $pdf->Cell(20, 10, mb_convert_encoding("Quantité", "ISO-8859-1", "UTF-8"), 1);
-$pdf->Cell(50, 10, "Prix", 1);
+$pdf->Cell(30, 10, "Prix", 1);
 $pdf->Ln();
 
 $pdf->SetFont('Arial', '', 12);
